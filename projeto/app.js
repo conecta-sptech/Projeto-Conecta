2b5eba079439769042cc24e2fef89fb6bf1b301e
leOhsantos <leonardo.santos191004@gmail.com>
2024-03-07 00:49:05 -0300

    projeto conecta

---
+++ projeto/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const PORT = 3333;
+
+const app = express();
+
+const indexRouter = require("./src/routes/index");
+const usuarioRouter = require("./src/routes/usuario");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/usuario", usuarioRouter);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORT}`);
+});
---
+++ projeto/src/database/config.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql2");
+
+const mySqlConfig = {
+    host: "localhost",
+    database: "",
+    user: "root",
+    password: ""
+};
+
+function execute(instruction) {
+    return new Promise(function (resolve, reject) {
+        const connection = mysql.createConnection(mySqlConfig);
+        connection.connect();
+        connection.query(instruction, function (error, results) {
+            connection.end();
+            if (error) {
+                reject(error);
+            }
+            resolve(results);
+        });
+        connection.on('error', function (error) {
+            return ("ERRO NO MySQL WORKBENCH: ", error.sqlMessage);
+        });
+    });
+}
+
+module.exports = { execute }
