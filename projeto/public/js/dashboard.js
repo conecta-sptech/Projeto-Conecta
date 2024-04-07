dad7b3764b1a8476d65146effee21ff2d23b6166
leOhsantos <leonardo.santos191004@gmail.com>
2024-04-07 05:34:29 -0300

    implementação dos modais

--- projeto/public/js/dashboard.js
+++ projeto/public/js/dashboard.js
@@ -1,5 +1,22 @@
 const sidebar = document.querySelector(".sidebar");
+const modalBackground = document.querySelector(".modal-background");
 
 function alterarMenu() {
     sidebar.classList.toggle("active");
+}
+
+function abrirModal(e) {
+    const id = e.getAttribute("data-id");
+    document.getElementById(id).classList.add("active");
+    modalBackground.classList.add("active");
+}
+
+function fecharModal(e) {
+    const id = e.getAttribute("data-id");
+    document.getElementById(id).classList.remove("active");
+    modalBackground.classList.remove("active");
+}
+
+function encerrarSessao() {
+    window.location.href = "../login.html";
 }
