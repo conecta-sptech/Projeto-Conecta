46a52d6d963857a6cf0dfd96734c4d3f98d7560d
leOhsantos <leonardo.santos191004@gmail.com>
2024-04-07 17:12:01 -0300

    criação das telas de cadastro de usuários e máquinas

--- projeto/public/js/dashboard.js
+++ projeto/public/js/dashboard.js
@@ -1,14 +1,55 @@
 const sidebar = document.querySelector(".sidebar");
 const modalBackground = document.querySelector(".modal-background");
 
+const telaNovoUsuario = document.querySelector(".novo-usuario-ct");
+const telaVisualizarUsuario = document.querySelector(".visualizar-usuario-ct");
+const btnNovoUsuario = document.getElementById("btnNovoUsuario");
+const btnVisualizarUsuario = document.getElementById("btnVisualizarUsuario");
+const inputNomeUsuario = document.getElementById("inputNomeUsuario");
+const selectTipoUsuario = document.getElementById("selectTipoUsuario");
+const inputEmailUsuario = document.getElementById("inputEmailUsuario");
+const inputSenhaUsuario = document.getElementById("inputSenhaUsuario");
+const inputConfirmarSenhaUsuario = document.getElementById("inputConfirmarSenhaUsuario");
+const line3ModalUsuario = document.getElementById("line3ModalUsuario");
+const line3ModalMaquina = document.getElementById("line3ModalMaquina");
+
+const telaNovaMaquina = document.querySelector(".nova-maquina-ct");
+const telaVisualizarMaquina = document.querySelector(".visualizar-maquina-ct");
+const btnNovaMaquina = document.getElementById("btnNovaMaquina");
+const btnVisualizarMaquina = document.getElementById("btnVisualizarMaquina");
+const inputNomeMaquina = document.getElementById("inputNomeMaquina");
+const inputIpMaquina = document.getElementById("inputIpMaquina");
+const inputRamMaquina = document.getElementById("inputRamMaquina");
+const inputCpuMaquina = document.getElementById("inputCpuMaquina");
+const inputDiscoMaquina = document.getElementById("inputDiscoMaquina");
+
 function alterarMenu() {
     sidebar.classList.toggle("active");
+}
+
+function resetarModalUsuario() {
+    btnNovoUsuario.classList.add("active");
+    btnVisualizarUsuario.classList.remove("active");
+    telaNovoUsuario.classList.add("active");
+    telaVisualizarUsuario.classList.remove("active");
+    line3ModalUsuario.style.display = "flex";
+}
+
+function resetarModalMaquina() {
+    btnNovaMaquina.classList.add("active");
+    btnVisualizarMaquina.classList.remove("active");
+    telaNovaMaquina.classList.add("active");
+    telaVisualizarMaquina.classList.remove("active");
+    line3ModalMaquina.style.display = "flex";
 }
 
 function abrirModal(e) {
     const id = e.getAttribute("data-id");
     document.getElementById(id).classList.add("active");
     modalBackground.classList.add("active");
+
+    resetarModalUsuario();
+    resetarModalMaquina();
 }
 
 function fecharModal(e) {
@@ -19,4 +60,36 @@ function fecharModal(e) {
 
 function encerrarSessao() {
     window.location.href = "../login.html";
+}
+
+function alterarFuncaoUsuario(e) {
+    if (e.classList.active != "active" && e.id == "btnNovoUsuario") {
+        btnNovoUsuario.classList.add("active");
+        btnVisualizarUsuario.classList.remove("active");
+        telaNovoUsuario.classList.add("active");
+        telaVisualizarUsuario.classList.remove("active");
+        line3ModalUsuario.style.display = "flex";
+    } else if (e.classList.active != "active" && e.id == "btnVisualizarUsuario") {
+        btnVisualizarUsuario.classList.add("active");
+        btnNovoUsuario.classList.remove("active");
+        telaVisualizarUsuario.classList.add("active");
+        telaNovoUsuario.classList.remove("active");
+        line3ModalUsuario.style.display = "none";
+    }
+}
+
+function alterarFuncaoMaquina(e) {
+    if (e.classList.active != "active" && e.id == "btnNovaMaquina") {
+        btnNovaMaquina.classList.add("active");
+        btnVisualizarMaquina.classList.remove("active");
+        telaNovaMaquina.classList.add("active");
+        telaVisualizarMaquina.classList.remove("active");
+        line3ModalMaquina.style.display = "flex";
+    } else if (e.classList.active != "active" && e.id == "btnVisualizarMaquina") {
+        btnVisualizarMaquina.classList.add("active");
+        btnNovaMaquina.classList.remove("active");
+        telaVisualizarMaquina.classList.add("active");
+        telaNovaMaquina.classList.remove("active");
+        line3ModalMaquina.style.display = "none";
+    }
 }
