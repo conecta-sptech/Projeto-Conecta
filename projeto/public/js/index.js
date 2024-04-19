cb083ebb823dadf0d80b2a92cec054ebdf7c8843
ezpmartins <enzo.martins@sptech.school>
2024-04-19 19:36:19 -0300

    Finalização da Responsividade

--- projeto/public/js/index.js
+++ projeto/public/js/index.js
@@ -8,12 +8,11 @@ function esconderRedesSociais(e) {
     e.classList.remove("active");
 }
 
-const mobileMenu = document.querySelector(".mobile-menu"),
-    navBar = document.querySelector(".navbar"),
-    navItems = document.querySelectorAll(".nav-item");
+const mobileMenu = document.querySelector(".mobile-menu");
+const navBar = document.querySelector(".navbar");
+const navItems = document.querySelectorAll(".nav-item");
 
-function midiaBotao() {
-    const midiaBotao = document.querySelector(".mobile-menu");
+function alternarMenuMobile() {
     mobileMenu.classList.toggle("active");
     navBar.classList.toggle("active");
     navItems.forEach((link, index) => {
@@ -21,11 +20,4 @@ function midiaBotao() {
             ? (link.style.animation = "")
             : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
     })
-}
-
-function handleMenuButton() {
-    navBar.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
-
-    
 }
