945b27a3bd28aac98f1021eb4d6895039867dea5
gabrielhgbarreto <gabriel.barreto@sptech.school>
2024-04-21 16:42:05 -0300

    der finalizado

--- projeto/src/database/bd-conecta.sql
+++ projeto/src/database/bd-conecta.sql
@@ -1,6 +1,5 @@
 -- Criação de Usuário no Banco de Dados
 CREATE USER "UserConecta"@"localhost" IDENTIFIED BY "Conecta2024";
-
 GRANT ALL PRIVILEGES ON Conecta.* to "UserConecta"@"localhost";
 
 -- Criação do Banco de Dados
@@ -40,31 +39,77 @@ CREATE TABLE Maquina (
 );
 
 -- Daqui em diante, precisa de validação.
-CREATE TABLE Leitura ( 
-idLeitura INT PRIMARY KEY AUTO_INCREMENT,
-RAM VARCHAR(45),
-Disco VARCHAR(45),
-CPU VARCHAR(45),
-Rede VARCHAR(45),
-MaquinaPertencente INT,
-FOREIGN KEY (MaquinaPertencente) 
-REFERENCES Maquinas(idMaquina)
+CREATE TABLE Componente(
+	idComponente INT PRIMARY KEY,
+    nomeComponente VARCHAR(25)
 );
 
-CREATE TABLE Alertas ( 
-idAlerta INT PRIMARY KEY AUTO_INCREMENT,
-Nome VARCHAR(45),
-Componente VARCHAR(45),
-FaixaAlerta VARCHAR(45)
+CREATE TABLE LeituraMemoria ( 
+	idLeituraMemoria INT PRIMARY KEY AUTO_INCREMENT,
+	memoriaDisponivel INT,
+	memoriaDedicadaSo INT,
+	memoriaTaxaTransferencia INT,
+    fkComponenteMemoria INT,
+		CONSTRAINT fkCM FOREIGN KEY (fkComponenteMemoria) REFERENCES Componente(idComponente),
+	fkMaquinaMemoria INT,
+		CONSTRAINT fkMM FOREIGN KEY (fkMaquinaMemoria) REFERENCES Maquina(idMaquina),
+	PRIMARY KEY (idLeituraMemoria, fkComponenteMemoria, fkMaquinaMemoria)
 );
 
-CREATE TABLE LogAlertas ( 
-idLogLeitura INT PRIMARY KEY AUTO_INCREMENT,
-fkLeitura INT,
-fkAlerta INT,
-DataHora DATETIME,
-FOREIGN KEY (fkLeitura) 
-REFERENCES Leitura(idLeitura),
-FOREIGN KEY (fkAlerta) 
-REFERENCES Alertas(idAlerta)
+CREATE TABLE LeituraCpu ( 
+	idLeituraCpu INT PRIMARY KEY AUTO_INCREMENT,
+	cpuDisponivel INT,
+	cpuTemperatura INT,
+	fkProcessosLeitura INT,
+	fkComponenteCpu INT,
+		CONSTRAINT fkCC FOREIGN KEY (fkComponenteCpu) REFERENCES Componente(idComponente),
+	fkMaquinaCpu INT,
+		CONSTRAINT fkMC FOREIGN KEY (fkMaquinaCpu) REFERENCES Maquina(idMaquina),
+	PRIMARY KEY (idLeituraCpu, fkComponenteCpu, fkMaquinaCpu)
+);
+
+CREATE TABLE Processo (
+	idProcesso INT,
+    fkLeituraCpu INT,
+		CONSTRAINT fkLP FOREIGN KEY (fkLeituraCpu) REFERENCES LeituraCpu(idLeituraCpu),
+    fkMaquinaProcesso INT,
+		CONSTRAINT fkMP FOREIGN KEY (fkMaquinaProcesso) REFERENCES Maquina(idMaquina),
+	PRIMARY KEY (idProcesso, fkLeituraCpu, fkMaquinaProcesso)
+);
+
+CREATE TABLE LeituraDisco ( 
+	idLeituraDisco INT PRIMARY KEY AUTO_INCREMENT,
+	discoDisponivel INT,
+	discoMemoriaSwap INT,
+    discoTemperatura INT,
+    fkComponenteDisco INT,
+		CONSTRAINT fkCD FOREIGN KEY (fkComponenteDisco) REFERENCES Componente(idComponente),
+	fkMaquinaDisco INT,
+		CONSTRAINT fkMD FOREIGN KEY (fkMaquinaDisco) REFERENCES Maquina(idMaquina),
+	PRIMARY KEY (idLeituraDisco, fkComponenteDisco, fkMaquinaDisco)
+);
+
+CREATE TABLE LeituraRede ( 
+	idLeituraRede INT PRIMARY KEY AUTO_INCREMENT,
+	redeDownload DECIMAL(5,2),
+	redeUpload DECIMAL(5,2),
+	redePing INT,
+    fkComponenteRede INT,
+		CONSTRAINT fkCR FOREIGN KEY (fkComponenteRede) REFERENCES Componente(idComponente),
+	fkMaquinaRede INT,
+		CONSTRAINT fkMR FOREIGN KEY (fkMaquinaRede) REFERENCES Maquina(idMaquina),
+	PRIMARY KEY (idLeituraRede, fkComponenteRede, fkMaquinaRede)
+);
+
+CREATE TABLE LogAlerta ( 
+	idAlerta INT PRIMARY KEY AUTO_INCREMENT,
+	corAlerta VARCHAR(45),
+	dataHoraAlerta VARCHAR(45),
+	fkComponenteAlerta INT,
+		CONSTRAINT fkCA FOREIGN KEY (fkComponenteAlerta) REFERENCES Componente(idComponente),
+	fkMaquinaAlerta INT,
+		CONSTRAINT fkMA FOREIGN KEY (fkMaquinaAlerta) REFERENCES Maquina(idMaquina),
+	fkMaquinaAlerta INT,
+		CONSTRAINT fkMA FOREIGN KEY (fkMaquinaAlerta) REFERENCES Maquina(idMaquina),
+	PRIMARY KEY (idAlerta, fkComponenteAlerta, fkMaquinaAlerta, fkMaquinaAlerta)
 );
