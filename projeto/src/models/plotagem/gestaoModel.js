ac4fb552d9b6a1a298e95d3be5c8878f358a3423
CamyAlves <camilly.souza@sptech.school>
2024-06-09 23:04:25 -0300

    Select gestao

--- projeto/src/models/plotagem/gestaoModel.js
+++ projeto/src/models/plotagem/gestaoModel.js
@@ -2,39 +2,44 @@ const database = require("../../database/config");
 
 function buscarMaquinas(idEmpresa){
     const instrucao = `
-        SELECT
-            m.idMaquina,
-            m.hostnameMaquina,
-            m.ociosidadeMaquina,
-            MAX(COALESCE(ld.dataHoraLeitura, lm.dataHoraLeitura, lr.dataHoraLeitura, lc.dataHoraLeitura)) AS ultimaLeitura,
-            CASE
-                WHEN MAX(COALESCE(ld.dataHoraLeitura, lm.dataHoraLeitura, lr.dataHoraLeitura, lc.dataHoraLeitura)) IS NULL
-                     OR MAX(COALESCE(ld.dataHoraLeitura, lm.dataHoraLeitura, lr.dataHoraLeitura, lc.dataHoraLeitura)) < NOW() - INTERVAL 24 HOUR
-                THEN 'Inativa'
-                ELSE 'Ativa'
-            END AS statusMaquina,
-            (
-                SELECT c.nomeComponente
-                FROM Componente c
-                INNER JOIN Alerta a ON c.idComponente = a.fkComponenteAlerta
-                WHERE a.fkMaquinaAlerta = m.idMaquina
-                ORDER BY a.dataHoraAlerta DESC
-                LIMIT 1
-            ) AS ultimoComponenteAlerta
-        FROM
-            Maquina m
-        LEFT JOIN
-            LeituraDisco ld ON m.idMaquina = ld.fkMaquinaDisco
-        LEFT JOIN
-            LeituraMemoria lm ON m.idMaquina = lm.fkMaquinaMemoria
-        LEFT JOIN
-            LeituraRede lr ON m.idMaquina = lr.fkMaquinaRede
-        LEFT JOIN
-            LeituraCpu lc ON m.idMaquina = lc.fkMaquinaCpu
-        WHERE
-            m.fkEmpresaMaquina = ${idEmpresa}
-        GROUP BY
-            m.idMaquina, m.hostnameMaquina, m.ociosidadeMaquina;
+       SELECT
+           m.idMaquina,
+           m.hostnameMaquina,
+           m.ociosidadeMaquina,
+           MAX(COALESCE(ld.dataHoraLeitura, lm.dataHoraLeitura, lr.dataHoraLeitura, lc.dataHoraLeitura)) AS ultimaLeitura,
+           CASE
+               WHEN MAX(COALESCE(ld.dataHoraLeitura, lm.dataHoraLeitura, lr.dataHoraLeitura, lc.dataHoraLeitura)) IS NULL
+                    OR MAX(COALESCE(ld.dataHoraLeitura, lm.dataHoraLeitura, lr.dataHoraLeitura, lc.dataHoraLeitura)) < DATEADD(HOUR, -24, GETDATE())
+               THEN 'Inativa'
+               ELSE 'Ativa'
+           END AS statusMaquina,
+           ultimoComponenteAlerta.nomeComponente AS ultimoComponenteAlerta
+       FROM
+           Maquina m
+       LEFT JOIN
+           LeituraDisco ld ON m.idMaquina = ld.fkMaquinaDisco
+       LEFT JOIN
+           LeituraMemoria lm ON m.idMaquina = lm.fkMaquinaMemoria
+       LEFT JOIN
+           LeituraRede lr ON m.idMaquina = lr.fkMaquinaRede
+       LEFT JOIN
+           LeituraCpu lc ON m.idMaquina = lc.fkMaquinaCpu
+       OUTER APPLY (
+           SELECT TOP 1
+               c.nomeComponente
+           FROM
+               Componente c
+           INNER JOIN
+               Alerta a ON c.idComponente = a.fkComponenteAlerta
+           WHERE
+               a.fkMaquinaAlerta = m.idMaquina
+           ORDER BY
+               a.dataHoraAlerta DESC
+       ) AS ultimoComponenteAlerta
+       WHERE
+           m.fkEmpresaMaquina = ${idEmpresa}
+       GROUP BY
+           m.idMaquina, m.hostnameMaquina, m.ociosidadeMaquina, ultimoComponenteAlerta.nomeComponente;
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
